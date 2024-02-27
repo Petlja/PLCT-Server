a7ca0982ee683e410db251226955774340ea24ab
vasiljevic <nebojsa.vasiljevic@gmail.com>
2024-02-27 21:03:12 +0100

    shnapshot: reading ai summaries

--- plct_server/content.py
+++ plct_server/content.py
@@ -1,26 +1,48 @@
 from dataclasses import dataclass
 import json
-from pathlib import Path
 from typing import Sequence, Optional
 from plct_cli.project_config import get_project_config, ProjectConfigError
 import os
+import glob
 from pydantic import BaseModel
 from click import UsageError
 import logging
 
+import yaml
+
 logger = logging.getLogger(__name__)
+
+
+class CourseAiContext(BaseModel):
+    summary: str
+    activity_summary: dict[str, str] # activity_id -> summary
 
 class CourseConfig(BaseModel):
     id: str
     title: str
-    static_website_root: Path
+    static_website_root: str
+    ai_context: CourseAiContext = None
+
 
 class ContentConfig(BaseModel):
-    course_project_dirs: Optional[Sequence[str]] = None
-    verbose: Optional[bool] = None
-    course_ids: Optional[list[str]] = None
-    course_dict: Optional[dict[str, CourseConfig]] = None # course_id -> course_config
 
+    # Original config values from the config file and/or CLI options
+    course_project_dirs: Sequence[str] = None
+    ai_context_dir: str = None
+    verbose: bool = None
+    base_path: str = None
+
+    # Attributes derived from the original config values
+    course_ids: list[str] = None
+    course_dict: dict[str, CourseConfig] = None # course_id -> course_config
+
+    def abspath(self, path: str) -> str:
+        """Return the absolute path of a file or directory relative to the base path."""
+        if os.path.isabs(path):
+            return path
+        if self.base_path is None:
+            return os.path.abspath(path)
+        return os.path.abspath(os.path.join(self.base_path, path))
 
 _current_content_config: ContentConfig = None
 
@@ -30,8 +52,8 @@ def get_content_config() -> ContentConfig:
         configure()
     return _current_content_config
 
-def configure(*, course_dirs: tuple[str] = None, verbose: bool = None) -> None:
-    global _current_content_config
+def configure(*, course_dirs: tuple[str] = None, verbose: bool = None,
+              ai_context_dir: str = None) -> None:
 
     content_config_file = os.environ.get("PLCT_SERVER_CONFIG_FILE")
 
@@ -41,6 +63,7 @@ def configure(*, course_dirs: tuple[str] = None, verbose: bool = None) -> None:
             with open(content_config_file, 'r') as file:
                 content_config_dict = json.load(file)
                 c_conf = ContentConfig(**content_config_dict)
+                c_conf.base_path = os.path.dirname(content_config_file)
         except (OSError, ValueError, TypeError)as e:
             logger.error(f"Error loading the configuration file '{content_config_file}': {e}")
     else: 
@@ -49,23 +72,28 @@ def configure(*, course_dirs: tuple[str] = None, verbose: bool = None) -> None:
     
     if c_conf is None:
         c_conf = ContentConfig()
-    
-    if verbose is not None:
-        c_conf.verbose = verbose
-    
-    if c_conf.verbose:
-        package_logger = logging.getLogger(__name__.split('.')[0])
-        package_logger.setLevel(logging.DEBUG)
 
-    if course_dirs is None and c_conf.course_project_dirs is not None:
-        course_dirs = c_conf.course_project_dirs
-    
     if course_dirs:
         c_conf.course_project_dirs = course_dirs
+    
+    if ai_context_dir:
+        c_conf.ai_context_dir = ai_context_dir
+
+    if verbose is not None:
+        c_conf.verbose = verbose
+
+    _apply_conf(c_conf)
+
+def _apply_conf(c_conf: ContentConfig) -> None:
+
+    if c_conf.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    
+    if c_conf.course_project_dirs:
         c_conf.course_ids = []
         c_conf.course_dict = {}
-        for folder in course_dirs:
-            if not Path(folder).is_dir():
+        for folder in c_conf.course_project_dirs:
+            if not os.path.isdir(folder):
                 raise UsageError(f"Folder {folder} does not exist.")
             try:
                 project_config = get_project_config(folder)
@@ -73,11 +101,12 @@ def configure(*, course_dirs: tuple[str] = None, verbose: bool = None) -> None:
                 raise UsageError(f"Folder {folder} is not a PLCT project.")
             if project_config.builder != "plct_builder":
                 raise UsageError(f"Folder {folder} is not a PLCT project.")
-            static_website_root = (Path(folder) / project_config.output_dir / 
-                                    project_config.builder / "static_website")
+            static_website_root = os.path.join(
+                c_conf.abspath(folder), project_config.output_dir, 
+                project_config.builder, "static_website")
 
-            course_config_path =  static_website_root / "course.json"
-            if not course_config_path.is_file():
+            course_config_path =  os.path.join(static_website_root, "course.json")
+            if not os.path.isfile(course_config_path):
                 raise UsageError(f"Course configuration file {course_config_path} does not exist.")
             
             with open(course_config_path, 'r') as file:
@@ -100,6 +129,36 @@ def configure(*, course_dirs: tuple[str] = None, verbose: bool = None) -> None:
             c_conf.course_ids.append(course_id)
             c_conf.course_dict[course_id] = course_config
 
-    logger.debug(f"Content configuration: {c_conf}")
+    if c_conf.ai_context_dir:
+        dir = c_conf.abspath(c_conf.ai_context_dir)
+        if not os.path.isdir(dir):
+            raise UsageError(f"AI context directory {dir} does not exist.")
+        
+        for course_id in c_conf.course_ids:
+            summary_yaml_path = f'{dir}/{course_id}/summary.yaml'
+            if os.path.isfile(summary_yaml_path):
+                course_config = c_conf.course_dict[course_id]
+                with open(f"{dir}/{course_id}/short-summary.txt", 'r') as file:
+                    course_summary = file.read()
+                ai_context = CourseAiContext(summary=course_summary, activity_summary={})
+                
+                with open(summary_yaml_path, 'r') as file:
+                    summary_yaml_str = file.read()
+
+                symmary_yaml = yaml.safe_load(summary_yaml_str)
+
+                for activity in symmary_yaml["activities"]:
+                    summaryPath = os.path.join(os.path.dirname(dir), activity["summaryPath"])
+                    with open(summaryPath, "r") as f:
+                        activity_summary = f.read()
+
+                    ai_context.activity_summary[activity["guid"]] = activity_summary
+
+                course_config.ai_context = ai_context
+
+    logger.debug(f"Content configuration: {c_conf} ")
+    global _current_content_config
     _current_content_config = c_conf
+
+
 
--- plct_server/run_server.py
+++ plct_server/run_server.py
@@ -12,14 +12,15 @@ logger = getLogger(__name__)
 @click.option("-h", "--host", default="127.0.0.1", help="Host to bind to")
 @click.option("-p", "--port", default=8000, help="Port to bind to")
 @click.option("-v", "--verbose", is_flag=True, help="Enable verbose logging")
-def serve(folders: tuple[str], host: str, port: int, verbose:bool) -> None:
+@click.option("-a", "--ai-context", help="Folder with AI context")
+def serve(folders: tuple[str], host: str, port: int, verbose:bool, ai_context:str) -> None:
     """Start the HTTP server for PLCT course(s).
     
     FOLDERS: The folders of PLCT projects to serve. If not provided, the current directory is used."""
 
     logger.info(f"folders: {folders}, host: {host}, port: {port}")
     
-    content.configure(course_dirs = folders, verbose = verbose)
+    content.configure(course_dirs = folders, verbose = verbose, ai_context_dir = ai_context)
 
     uvicorn.run(app, host=host, port=port) 
 
