7c2461b952bac1a2df71d9fdd10934f01507f9e5
Jovan Maric <33927662+maric993@users.noreply.github.com>
2024-08-18 17:38:45 +0200

    Update dev-server.cmd

--- dev-server.cmd
+++ dev-server.cmd
@@ -1,5 +1,4 @@
 @ECHO OFF
-call poetry shell
 IF NOT EXIST dev-server.json COPY dev-server.sample.json dev-server.json
 set PLCT_SERVER_CONFIG_FILE=dev-server.json
-uvicorn plct_server.ui_main:app --reload --host 127.0.0.1 --port 8000
+poetry run uvicorn plct_server.ui_main:app --reload --host 127.0.0.1 --port 8000
