8ed0f4f649431f707c3039d1f9b6a1d2968c5b7d
Jovan Maric <33927662+maric993@users.noreply.github.com>
2024-08-20 11:51:24 +0200

    more cleaning

--- plct_server/ai/engine.py
+++ plct_server/ai/engine.py
@@ -3,11 +3,11 @@ import logging
 import chromadb
 import tiktoken
 
+from tiktoken import Encoding
 from typing import AsyncIterator
 from openai import AsyncAzureOpenAI
-from pydantic import BaseModel
-from tiktoken import Encoding
 from .context_dataset import ContextDataset
+from .query_context import QueryContext, QueryError
 from . import OPENAI_API_KEY
 
 from .prompt_templates import ( 
@@ -63,37 +63,6 @@ RESPONSE_MAX_TOKENS = 2000
 PREPROCESS_QUERY_MAX_TOKENS = 1000
 LLM_MODEL = "gpt-35-turbo"
 LLM_EMBEDDING_MODEL = "text-embedding-ada-002"
-class QueryError(Exception):
-    pass
-
-class QueryContext(BaseModel):
-    chunk_metadata : list[dict[str,str]] = []
-    system_message : str = ""
-    token_size : dict[str,int] = {}
-
-    def add_chunk_metadata(self, chunk_metadata: dict[str,str], distance: float):
-        chunk_metadata["distance"] = str(distance)
-        self.chunk_metadata.append(chunk_metadata)
-
-    def get_all_chunk_activity_keys(self) -> str:
-        return [item["activity_key"] for item in self.chunk_metadata]
-            
-    def add_encoding_length(self, name: str, message: str, encoding: Encoding) -> None:
-        if name not in self.token_size:
-            self.token_size[name] = 0
-        self.token_size[name] += len(encoding.encode(message))
-    
-    def get_encoding_length(self) -> int:
-        return sum(self.token_size.values())
-    
-    def log_encoding_length(self) -> None:
-        for name, size in self.token_size.items():
-            logger.debug(f"Encoding length for {name}: {size}")
-
-    def add_system_message_parts(self,parts: list[dict[str, str]], encoding: Encoding) -> None:
-        for part in parts:
-            self.add_encoding_length(part["name"], part["message"], encoding)
-            self.system_message += part["message"]
 
 class AiEngine:
     def __init__(self, base_url: str):
@@ -165,7 +134,7 @@ class AiEngine:
             dimensions=dimensions
         )
         return response.data[0].embedding
-    #creates a more elaborated request using query, condensed history and latest history
+
     async def preprocess_query(self, history: list[tuple[str,str]], query: str, course_key: str, activity_key: str, condensed_history: str) -> str:
       
         course_summary, lesson_summary = self.ctx_data.get_summary_texts(
